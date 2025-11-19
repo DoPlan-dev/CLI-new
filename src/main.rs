0c645989a99606fe75b3916b43a67c41e081643e
iDorgham <yasser.dorgham@gmail.com>
2025-11-19 10:29:44 +0200

    chore: release 1.0.9-beta

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use doplan::commands::{discuss, generate, plan, implement, next, progress, phase
 #[derive(Parser)]
 #[command(name = "doplan")]
 #[command(about = "Project workflow automation tool", long_about = None)]
-#[command(version = "1.0.8-beta")]
+#[command(version = "1.0.9-beta")]
 struct Cli {
     #[command(subcommand)]
     command: Option<Commands>,
